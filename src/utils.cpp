7b8cb4593431ffa59e13a13f7e2c14eac55b18fa
Diego D <ddarriba@users.noreply.github.com>
2022-02-19 11:56:39 +0100

    float to word

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,56 +1,124 @@
 #include "utils.h"
 #include <cmath>
 #include <cstdio>
+#include <cassert>
+#include <iostream>
 
 namespace mips_sim
 {
 
-uint32_t Utils::float_to_word(float f)
+  static void _float_to_word(double f,
+                             uint32_t te, uint32_t tm,
+                             uint32_t word[])
+  {
+    uint64_t v = 0;
+    uint32_t excess = (1<<(te-1))-1;
+
+    uint32_t bitcount = te + tm + 1;
+    assert(bitcount == 32 || bitcount == 64);
+
+    uint64_t sign;
+    uint64_t exp;
+    uint64_t mantissa;
+
+    uint64_t integer_part, decpart;
+    double decimal_part;
+
+    sign = f < 0;
+    sign <<= (bitcount-1);
+    f = fabs(f);
+
+    integer_part = static_cast<uint64_t>(floor(f));
+    decimal_part = f - static_cast<double>(integer_part);
+
+    /* convert decimal */
+    decpart = 0;
+    for (size_t i=0; i<tm; ++i)
+    {
+      decimal_part *= 2;
+      if (decimal_part >= 1)
+      {
+        decpart |= 1;
+        decimal_part -= 1.0f;
+      }
+      decpart <<= 1LL;
+    }
+
+    decpart <<= bitcount - tm - 1;
+    mantissa = decpart;
+
+    exp = excess;
+    if (integer_part > 1)
+    {
+      while (integer_part > 1)
+      {
+         mantissa >>= 1;
+         mantissa &= (1ULL << (bitcount-1))-1; //0x7fffffffffffffff;
+         mantissa |= ((integer_part << (bitcount-1)) & (1ULL<<(bitcount-1)));//0x8000000000000000);
+         integer_part >>= 1;
+         exp ++;
+      }
+    }
+    else if (integer_part < 1)
+    {
+      printf("Not implemented IEEE754 for values < 1\n");
+      exit(1);
+    }
+
+    v = sign;
+    v |= ((exp & ((11ULL<<te)-1)) << (bitcount-te-1));
+    v |= ((mantissa >> (bitcount-tm)) & ((1ULL<<tm)-1));
+
+    if (bitcount == 64)
+    {
+      word[1] = static_cast<uint32_t>(v & 0xFFFFFFFF);
+      word[0] = static_cast<uint32_t>((v & 0xFFFFFFFF00000000)>>32);
+    }
+    else
+    {
+      word[0] = static_cast<uint32_t>(v & 0xFFFFFFFFLL);
+    }
+  }
+
+  template  <>
+  void Utils::float_to_word(double f, uint32_t word[])
+  {
+    _float_to_word(f, 11, 52, word);
+  }
+
+  template  <>
+  void Utils::float_to_word(float f, uint32_t word[])
+  {
+    _float_to_word( static_cast<double>(f), 8, 23, word);
+  }
+
+
+float Utils::word_to_float(uint32_t w)
 {
-  uint32_t v = 0;
-
+  float v;
+  float addv;
   uint32_t tm = 23, te = 8, excess = 127;
-  uint32_t sign;
-  uint32_t exp;
-  uint32_t mantissa;
+  uint32_t sign, exp, mantissa;
 
-  sign = static_cast<uint32_t>((f < 0) << 31);
-  f = fabsf(f);
+  sign = (w >> 31);
+  mantissa = (w & ((1 << tm)-1));
+  exp = ((w >> tm) & ((1 << te)-1)) - excess;
 
-  uint32_t integer_part = static_cast<uint32_t>(floorf(f));
-  float decimal_part = f - static_cast<float>(integer_part);
-
-  /* convert decimal */
-  uint32_t decpart = 0;
-  for (size_t i=0; i<tm; ++i)
+  addv = 1.0f;
+  v = 1.0;
+  for (int i=0; i<tm; i++)
   {
-    decimal_part *= 2;
-    if (decimal_part > 1)
+    if ((mantissa >> (tm-i)) & 1)
     {
-      decpart |= 1;
-      decimal_part -= 1.0f;
+      v += addv;
     }
-    decpart <<= 1;
-  }
-  decpart <<= 31 - tm;
-
-  mantissa = decpart;
-  exp = excess;
-  if (integer_part > 1)
-  {
-    while (integer_part > 1)
-    {
-       mantissa >>= 1;
-       mantissa &= 0x7fffffff;
-       mantissa |= ((integer_part << 31) & 0x80000000);
-       integer_part >>= 1;
-       exp ++;
-    }
+    addv /= 2;
   }
 
-  v = sign;
-  v |= ((exp & ((1<<te)-1)) << (31-te));
-  v |= ((mantissa >> (32-tm)) & ((1<<tm)-1));
+  v *= powf(2, exp);
+
+  if (sign == 1)
+    v *= -1;
 
   return v;
 }
--- src/utils.h
+++ src/utils.h
@@ -9,7 +9,13 @@ namespace mips_sim
 class Utils
 {
 public:
-  static uint32_t float_to_word(float f);
+
+  //static uint32_t float_to_word(float f);
+  template  <typename T>
+  static void float_to_word(T f, uint32_t word[]);
+
+  static float word_to_float(uint32_t f);
+  //static double word_to_double(uint32_t f);
 };
 
 } /* namespace */
